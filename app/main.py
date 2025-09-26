e7d5ca52a1d22b350835f1d78c4bc41ae38831c6
SEAN MALONEY - STUDENT <g00420811@atu.ie>
2025-09-26 12:31:46 +0100

    Added update user

--- app/main.py
+++ app/main.py
@@ -19,10 +19,29 @@ def get_user(user_id: int):
         if u.user_id == user_id:
             return u
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
- 
+#Add user
 @app.post("/api/users", status_code=status.HTTP_201_CREATED)
 def add_user(user: User):
     if any(u.user_id == user.user_id for u in users):
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="user_id already exists")
     users.append(user)
     return user
+#Update User
+@app.put("/api/users/{user_id}", status_code=status.HTTP_201_CREATED)
+def update_user(user_id: int, updated_user: User):
+    for i, u in enumerate(users):
+        if u.user_id == user_id:
+            users[i] = updated_user
+            return updated_user
+    if any(u.user_id == update_user.user_id for u in users):
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="user_id doesn't exist")
+
+
+
+#Delete user
+#@app.del("/api/users", status_code=status.HTTP_201_CREATED)
+#def del_user(user: User):
+#    if any(u.user_id == user.user_id for u in users):
+#        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="user_id doesn't exist")
+ #   users.append(user)
+ #   return user
