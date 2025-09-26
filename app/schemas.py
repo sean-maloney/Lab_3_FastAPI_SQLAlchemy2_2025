7f095f00e1171b689c6e981cec36f14df7bc86eb
SEAN MALONEY - STUDENT <g00420811@atu.ie>
2025-09-26 13:05:57 +0100

    Added Student String

--- app/schemas.py
+++ app/schemas.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel, EmailStr, constr, conint
 
 class User(BaseModel):
     user_id: int
+    student_id: constr(pattern=r"^S\d{7}$")
     name: constr(min_length=2, max_length=50)
     email: EmailStr
     age: conint(gt=18)
