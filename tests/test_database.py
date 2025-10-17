cb631208ba6b81ed17d583697c4403f47abf04db
sean-maloney <118452234+sean-maloney@users.noreply.github.com>
2025-10-17 10:37:54 +0000

    Changed lab to reflect the most recent lab, this was a duplicate of last weeks lab

---
+++ tests/test_database.py
@@ -0,0 +1,30 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.main import app, get_db
+from app.models import Base
+
+TEST_DB_URL = "sqlite+pysqlite:///:memory:"
+engine = create_engine(TEST_DB_URL, connect_args={"check_same_thread": False})
+TestingSessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
+Base.metadata.create_all(bind=engine)
+
+@pytest.fixture
+def client():
+    def override_get_db():
+        db = TestingSessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
+    app.dependency_overrides[get_db] = override_get_db
+    with TestClient(app) as c:
+# hand the client to the test
+        yield c
+# --- teardown happens when the 'with' block exits ---
+def test_create_user(client):
+    r = client.post("/api/users",
+json={"name":"Paul","email":"pl@atu.ie","age":25,"student_id":"S1234567"})
+    assert r.status_code == 201
