d1ba41557794366272a5502ebff7df976706858f
sean-maloney <118452234+sean-maloney@users.noreply.github.com>
2025-10-17 10:54:22 +0000

    minor changes

--- app/schemas.py
+++ app/schemas.py
@@ -1,8 +1,10 @@
 # app/schemas.py
 from typing import Annotated
 from pydantic import BaseModel, EmailStr, Field, StringConstraints, ConfigDict
+
 NameStr = Annotated[str, StringConstraints(min_length=2, max_length=50)]
 StudentId = Annotated[str, StringConstraints(pattern=r"^S\d{7}$")]
+
 class UserCreate(BaseModel):
     name: NameStr
     email: EmailStr
